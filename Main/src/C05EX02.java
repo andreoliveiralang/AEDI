2aa37ba22eec808d52c33c916796cd9ffa79e0b4
andreoliveiralang <andreoliveiralang@gmail.com>
2026-03-13 21:20:58 -0300

    commit

--- Main/src/C05EX02.java
+++ Main/src/C05EX02.java
@@ -4,7 +4,6 @@
 //Matricula:D29079
 
 import java.util.Scanner;
-<<<<<<< HEAD
 public class C05EX02 {
     public static void main(String[] args){
         
@@ -25,27 +24,4 @@ public class C05EX02 {
 
         keyboard.close();
    }
-=======
-public class C05EX02 {    
-     public static void main(String[] args){
-            Double valordePi, valorArea ,valorVolume;
-            Scanner keyboard = new Scanner (System.in);
-            System.out.print("Digite o raio da esfera (R): ");
-            double raioEsfera = keyboard.nextDouble();
-            
-            valordePi = 3.1416;
-
-            valorArea = 4* valordePi*raioEsfera*(Math.pow(raioEsfera,2));
-
-            System.out.printf("resultado da Área:%.1f " ,valorArea);
-
-            System.out.println();
-            
-            valorVolume = 4.0/3.0 * valordePi*(Math.pow(raioEsfera,3));
-
-            System.out.printf("resultado do Volume: %.1f", valorVolume);
-            
-            keyboard.close(); 
-    }
->>>>>>> 93ad8f3f875af4cf31cefeedd82884197a7d7737
 }
