070bf6e6e71504c11c94c2da52d4bb66aff4ae7e
André Lang <andreoliveiralang@gmail.com>
2026-04-22 09:41:28 -0300

    commit

--- Main/src/ETAPA2/C09EX01.java
+++ Main/src/ETAPA2/C09EX01.java
@@ -1,20 +1,16 @@
-//Nome: André Luiz De Oliveira Lang
-//Curso: Ciências da computação
-//Disciplina: Algoritmos e Estrutura de Dados I
-//Matricula:D29079
+
+// MODELO PARA SÉRIES MATEMATICAS SOMATÓRIO. //
+
 package ETAPA2;
 public class C09EX01 {
-     public static void main(String[] args) {
+ public static void main(String[] args) {
    double num, den, serie=0;
    for (int aux=1; aux<= 37; aux++) {  // ? = quantidade de termos da série
-     num = Math.pow(aux,2);//? = implementação da variação do numerador
-     den = Math.pow(7, 2) + 1 ;//? = implementação da variação do denominador
+     num = (38 - aux) * (39 -aux);//? = implementação da variação do numerador
+     den = aux;//? = implementação da variação do denominador
      serie += num / den;
    }
+   
    System.out.print("Valor da Série = "+serie);
  }
-    
 }
-
-
-//totvs Estagio bdmg mercantil tribunais accentury
---
+++ Main/src/ETAPA2/C09EX02.java
@@ -0,0 +1,20 @@
+//Nome: André Luiz De Oliveira Lang
+//Curso: Ciências da computação
+//Disciplina: Algoritmos e Estrutura de Dados I
+//Matricula:D29079
+package ETAPA2;
+public class C09EX02 {
+ public static void main(String[] args) {
+   double num, den, serie=0;
+   for (int aux=1; aux<= 100; aux++) {  // ? = quantidade de termos da série
+     num = aux ;//? = implementação da variação do numerador
+     den = aux * 1-1;//? = implementação da variação do denominador
+     serie += num / den;
+   }
+   
+   System.out.print("Valor da Série = "+serie);
+ }
+}
+
+
+//totvs Estagio bdmg mercantil tribunais accentury
