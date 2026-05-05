9a9779fd1024add91193ba960e12c7f1d2026683
André Lang <andreoliveiralang@gmail.com>
2026-05-05 11:13:14 -0300

    commit

---
+++ Main/src/ETAPA2/C08EX30.java
@@ -0,0 +1,21 @@
+package ETAPA2;
+import java.util.Scanner;
+public class C08EX30 
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int x,n;
+        
+        System.out.println("Insira o valor de X: ");
+        x = keyboard.nextInt();
+        System.out.println("Insira o valor de N: ");
+        n = keyboard.nextInt();
+        
+            for (int aux2=1; aux2 <=10; aux2++)
+            {
+                
+            }
+        
+    }  
+}
